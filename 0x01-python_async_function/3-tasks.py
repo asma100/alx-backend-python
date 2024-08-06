169824e9e9002b4bb2a0b7ab34b0d1dca1220940
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-08-05 18:24:01 -0700

    task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Creates an asyncio.Task for the wait_random coroutine"""
+    return asyncio.create_task(wait_random(max_delay))
