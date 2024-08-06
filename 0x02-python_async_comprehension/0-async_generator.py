60c44c3b294773aafde3d34f0a741976bab0cc30
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-08-06 16:41:28 -0700

    task 0 ed

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """module"""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator() -> float:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Waits for 1s then ggives random number """
     for _ in range(10):
         await asyncio.sleep(1)
